2d6564863b64816821b773eac40d520bef7acf05
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2023-10-15 16:19:54 +0300

    update

--- src/components/Statistics/StatsListItem.jsx
+++ src/components/Statistics/StatsListItem.jsx
@@ -1,11 +1,7 @@
+import { getRandomHexColor } from 'helpers/getRandomHexColor';
 import css from './Statistics.module.css';
 
 export const StatsListItem = ({ dataItem: { label, percentage } }) => {
-  const getRandomHexColor = () => {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  };
   return (
     <li style={{ backgroundColor: getRandomHexColor() }} className={css.item}>
       <span className={css.label}>{label}</span>
