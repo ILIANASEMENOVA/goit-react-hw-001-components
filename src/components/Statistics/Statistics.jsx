199197bfd6bceaafe2a24c8b54881a0fa0666962
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2023-10-18 22:04:15 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { StatsListItem } from './StatsListItem';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title}>{title}</h2> : ''}
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(item => (
